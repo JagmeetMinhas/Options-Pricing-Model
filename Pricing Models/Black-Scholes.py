4d8fbdb4d8a5bd2382e75108b097eb9a6bed3917
JagmeetMinhas <swiftblade184@gmail.com>
2024-09-02 16:44:15 -0400

    Develop profit calculator subprogram

--- Pricing Models/Black-Scholes.py
+++ Pricing Models/Black-Scholes.py
@@ -11,6 +11,11 @@ class blackScholesEquation:
         self.volatility = volatility
         self.optionType = optionType
 
+    def getStrikePrice(self):
+        '''To be used for per share profit calculation'''
+        return self.currentPrice
+
+
     def d1Calculation(self):
         numerator = np.log(self.currentPrice/self.strikePrice) + (self.riskFreeRate + (((self.volatility**2)/2)) * self.yearsToExpiration)
         denominator =  self.volatility * (self.yearsToExpiration**0.5)
@@ -32,6 +37,24 @@ class blackScholesEquation:
 def main():
     model = blackScholesEquation(50, 45, 0.5, 0.05, 0.20, "put")
 
-    print(model.priceCalculation())
+    print(f'The fair market premium for this option is ${model.priceCalculation():.2f}.')
+
+    """Does profit change if the option is a call or a put?"""
+
+    profitCalc = input("Would you like to calculate your theoretical profit? (y/n)\n")
+
+    while profitCalc == "y":
+        price = float(input("Great! Provide the price you believe the asset will be at when the option will be exercised:\n"))
+        while price < 0:
+            raise ValueError("A price value can't be negative.")
+        profit = price - model.getStrikePrice() - model.priceCalculation()
+        if profit < 0:
+            print(f"At a price of ${price}, ${-profit:.2f} will be lost per share.")
+        elif profit > 0:
+            print(f"At a price of ${price:.2f}, ${profit:.2f} will be made per share.")
+        else:
+            print("No profit will be made at this price.")
+
+        profitCalc = input("Would you like to try another theoretical profit calculation? (y/n)\n")
 
 main()
