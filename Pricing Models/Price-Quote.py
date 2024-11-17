0aa1560cad1bb5d12a85a4e2465bda15e233f3e1
JagmeetMinhas <swiftblade184@gmail.com>
2024-11-17 17:11:35 -0500

    Updated API call

--- Pricing Models/Price-Quote.py
+++ Pricing Models/Price-Quote.py
@@ -1,1 +1,9 @@
 #Yahoo Finance API
+from .Black_Scholes import blackScholesEquation
+import yfinance as yf
+
+ticker = yf.Ticker("AAPL")
+
+opt_chain = ticker.option_chain()
+
+print(opt_chain.calls)
